7eead3fc25ee86b14d24e8080c3f88f6f3f038ae
TesterSzymoon <47006674+TesterSzymoon@users.noreply.github.com>
2026-09-07 01:34:21 +0200

    include raylib, test version of main

--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@
  * Main function
  */
 
-#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "raylib.h"
 
 #include "const.h"
 #include "errlog.h"
+#include "mem.h" /* get rid of that, tests */
 #include "types.h"
 
 /* main function */
@@ -24,6 +25,27 @@ int main(int argc, char** argv) {
 	if (!mem) return errftl("couldn't allocate sufficient memory for the system", 2);
 
 	/* main program loop */
+
+	SetTraceLogLevel(LOG_ERROR); /* we only want to get notified of errors */
+
+	InitWindow(DEFAULT_BASE_W * DEFAULT_BASE_S, DEFAULT_BASE_H * DEFAULT_BASE_S, ETA_NAME);
+	SetTargetFPS(60);
+
+	printf("allocated %d bytes\n", MEM_AMNT * sizeof(hword));
+
+	/* tests */
+	mem_hw_w(mem, 0, (hword) 0x0f);
+	printf("val: %.2x\n", mem_hw_r(mem, 0));
+	/* ----- */
+
+	while(!WindowShouldClose()) {
+		BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("eta-tau!", 50, 50, 20, DARKGRAY);
+        EndDrawing();
+	}
+
+	CloseWindow();
 
 	free(mem);
 
